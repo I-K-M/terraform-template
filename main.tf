12b932ffeb2ba20c8c182d0fa1734c58e9e93b80
I-K-M <ik-m@outlook.fr>
2025-07-12 12:40:27 +0100

    basic infra: 1 vpc, 2 subnets priv+pub, 1 ec2

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.20.0.0/16"
+}
+
+# Public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.20.1.0/24"
+  availability_zone       = "eu-west-3a"
+  map_public_ip_on_launch = true
+}
+
+# Private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.20.2.0/24"
+  availability_zone = "eu-west-3a"
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route tables
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Link table to subnets
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# EC2 in public subnet
+resource "aws_instance" "web" {
+  ami           = "ami-01032886170466a16"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "WebServer"
+  }
+}
