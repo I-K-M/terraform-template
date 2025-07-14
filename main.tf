84993c5af86b76f9a9d2382db4ddb67eb713a6f5
I-K-M <ik-m@outlook.fr>
2025-07-14 22:29:11 +0100

    s3 bucket endpoint + NAT gateway added

--- main.tf
+++ main.tf
@@ -27,6 +27,18 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+# Elastic IP
+resource "aws_eip" "nat_eip" {
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
 # Route tables
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
@@ -39,6 +51,11 @@ resource "aws_route_table" "rt" {
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 }
 
 # Link table to subnets
@@ -52,13 +69,25 @@ resource "aws_route_table_association" "b" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-# EC2 in public subnet
+# EC2 in private subnet
 resource "aws_instance" "web" {
   ami           = "ami-01032886170466a16"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.private_subnet.id
 
   tags = {
     Name = "WebServer"
   }
+}
+
+# S3 bucket endpoint
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.eu-west-3.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.rt.id,
+    aws_route_table.private_rt.id
+  ]
 }
